170394e22008a2875bbdc7ffc5a88bf239c685ff
Your Name <you@example.com>
2026-06-14 20:20:51 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let cors = require("cors");
+
+let bookingRoutes = require("./routes/bookingRoutes");
+require("dotenv").config();
+let app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/",bookingRoutes);
+
+let PORT = process.env.port || 9000;
+app.listen(PORT, () => {
+    console.log("server running on port " + PORT);
+});    
